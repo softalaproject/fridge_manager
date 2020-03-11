0363e22bf12df19467a13863246389ac64f4dfdb
Samu <Samu@email.com>
2020-03-11 11:18:24 +0200

    beer function added for another language

--- restmanager/frontend/views.py
+++ restmanager/frontend/views.py
@@ -7,18 +7,34 @@ from django.contrib import messages
 from django.http import JsonResponse
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'frontend/index.html')
 
-#GET SLACK TOKEN HERE
+
+# GET SLACK TOKEN HERE
 load_dotenv()
 client = slack.WebClient(token=os.getenv("SLACK_TOKEN"))
 
-#Beer function
+# Beer function
+
+
 def nobeer(request):
     client.chat_postMessage(
         channel='#general',
         text="Saunatilasta on juomat loppu. Haeppa lisää!"
     )
     messages.success(request, 'Viesti lähetetty Slack-kanavalle')
+    return JsonResponse({'success': True})
+
+# Another beer function
+
+
+def another_beer(request):
+    client.chat_postMessage(
+        channel="#general",
+        text="Beer is out"
+    )
+    messages.success(request, 'Message sent')
     return JsonResponse({'success': True})
