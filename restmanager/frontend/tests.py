682c5ce7808aaccb57a941458687fd6eeb16fbb9
Ari Aaltonen <aaltonengithub@gmail.com>
2020-03-22 07:14:26 +0200

    Edited StringsTests to match new strings.py

--- restmanager/frontend/tests.py
+++ restmanager/frontend/tests.py
@@ -4,12 +4,11 @@ from . import strings
 
 class StringsTests(unittest.TestCase):
     def test_strings(self):
-        self.assertEqual('Saunatilasta on juomat loppu. Haeppa lisää!', strings.SLACKMESSAGE_1)
+        self.assertEqual('Saunatilan kaappi on tyhjä.', strings.SLACK_MESSAGE_1)
         self.assertEqual('#general', strings.CHANNEL_NAME_1)
-        self.assertEqual('Viesti lähetetty Slack-kanavalle', strings.SUCCESS_MSG_1)
-        self.assertEqual('Beer is out', strings.SLACKMESSAGE_2)
-        self.assertEqual('#general', strings.CHANNEL_NAME_2)
-        self.assertEqual('Message sent', strings.SUCCESS_MSG_2)
+        self.assertEqual('Saunatilan kaappi täytetty.', strings.SLACK_MESSAGE_2)
+        self.assertEqual('Täyttäminen vaiheessa.', strings.SLACK_MESSAGE_3)
+        self.assertEqual('Otit vastuun täyttämisestä, viesti laitettu Slackiin.', strings.SUCCESS_MSG_3)
 
 
 if __name__ == '__main__':
