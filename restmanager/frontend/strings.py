f8cfa06ae4ee763ab0b6d4a7f45930223798a8c2
Ari Aaltonen <aaltonengithub@gmail.com>
2020-03-22 07:08:13 +0200

    Removed redundant strings

--- restmanager/frontend/strings.py
+++ restmanager/frontend/strings.py
@@ -1,18 +1,11 @@
 
-# Slackbot_1
-SLACKMESSAGE_1 = "Saunatilasta on juomat loppu. Haeppa lisää!"
+# Slack_bot_1
+SLACK_MESSAGE_1 = "Saunatilan kaappi on tyhjä."
 CHANNEL_NAME_1 = '#general'
-SUCCESS_MSG_1 = 'Viesti lähetetty Slack-kanavalle'
 
-# Slackbot_2 
-SLACKMESSAGE_2 = "Beer is out"
-CHANNEL_NAME_2 = "#general"
-SUCCESS_MSG_2 = 'Message sent'
+# Slack_bot 2
+SLACK_MESSAGE_2 = "Saunatilan kaappi täytetty."
 
-# Slackbot 3
-SLACKMESSAGE_3 = "Beer is back on the menu. Kaappi täytetty."
-SUCCESS_MSG_3 = "Toiminto kuitattu Slackiin."
-
-# Slackbot 4
-SLACKMESSAGE_4 = "Täyttäminen vaiheessa."
-SUCCESS_MSG_4 = "Otit vastuun täyttämisestä, viesti laitettu Slackiin."
+# Slack_bot 3
+SLACK_MESSAGE_3 = "Täyttäminen vaiheessa."
+SUCCESS_MSG_3 = "Otit vastuun täyttämisestä, viesti laitettu Slackiin."
