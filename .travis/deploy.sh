1d9e3df54a46fae5fe87deefe7f94487bd389cbb
nikke845 <nvuorivirta@gmail.com>
2020-04-01 12:25:18 +0300

    tracing errors

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,8 +11,8 @@ git push deploy master
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh apps@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
-  pip install -r requirements.txt
-  python restmanager/manage.py migrate
-  python restmanager/manage.py runserver
+  pip3 install -r requirements.txt
+  python3 restmanager/manage.py migrate
+  python3 restmanager/manage.py runserver
 
 EOF
