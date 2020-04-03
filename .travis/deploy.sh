ab180a7226f373a8a11eabf279e4e26d4e69bd31
Ari Aaltonen <aaltonengithub@gmail.com>
2020-04-03 11:35:12 +0300

    Add #

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,7 +7,7 @@ ssh-add .travis/id_rsa # Add the private key to SSH
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy dev
-
+# 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh apps@$IP -p $PORT <<EOF
   pkill -f runserver
