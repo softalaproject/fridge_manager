26a5acc5b123d752f7005abae6817f2445d9bd9d
Ari Aaltonen <aaltonengithub@gmail.com>
2020-04-03 12:56:35 +0300

    Add git reset --hard

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,7 +13,7 @@ ssh apps@$IP -p $PORT <<EOF
   pkill -f runserver
   cd $DEPLOY_DIR
   git fetch origin
-  git pull origin dev
+  git reset --hard origin/dev
   pip3 install -r requirements.txt
   python3.6 restmanager/manage.py runserver $IP2:8080
 EOF
