a0e31bfefbf6d2537ba3cfca2ca7bc07d0abd3a6
Ari Aaltonen <aaltonengithub@gmail.com>
2020-04-10 10:54:55 +0300

    Add local ip to settings.py, fix test

--- restmanager/frontend/tests.py
+++ restmanager/frontend/tests.py
@@ -6,7 +6,7 @@ class StringsTests(unittest.TestCase):
     def test_strings(self):
         self.assertEqual('Saunatilan kaappi on tyhjä.', strings.SLACK_MESSAGE_1)
         self.assertEqual('#general', strings.CHANNEL_NAME_1)
-        self.assertEqual('Saunatilan kaappi täytetty.', strings.SLACK_MESSAGE_2)
+        self.assertEqual('Saunatilan kaappi on täytetty.', strings.SLACK_MESSAGE_2)
         self.assertEqual('Täyttäminen vaiheessa.', strings.SLACK_MESSAGE_3)
         self.assertEqual('Otit vastuun täyttämisestä, viesti laitettu Slackiin.', strings.SUCCESS_MSG_3)
 
