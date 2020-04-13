e66bc5105489b4c2976d4485d9c40c394ad64034
Lauri Leinonen <leinonen.lauri7@gmail.com>
2020-04-13 17:02:09 +0300

    django reads only one testsuite

--- restmanager/frontend/tests.py
+++ restmanager/frontend/tests.py
@@ -1,15 +1,30 @@
-import unittest
+from .views import fridge, manage, post_beer, post_no_beer
+from django.urls import reverse, resolve
+from unittest import TestCase
 from . import strings
 
+class UrlsTest(TestCase):
+    def fridge_test(self):
+        url = reverse('')
+        self.assertEquals(resolve(url).func, fridge)
 
-class StringsTests(unittest.TestCase):
+    def manage_test(self):
+        url = reverse('manage')
+        self.assertEquals(resolve(url).func, manage)
+
+    def beer_test(self):
+        url = reverse('api/beer')
+        self.assertEquals(resolve(url).func, post_beer)
+
+    def no_beer_test(self):
+        url = reverse('api/no_beer')
+        self.assertEquals(resolve(url).func, post_no_beer)
+
+
+class UnitTests(TestCase):
     def test_strings(self):
         self.assertEqual('Saunatilan kaappi on tyhjä.', strings.SLACK_MESSAGE_1)
         self.assertEqual('#general', strings.CHANNEL_NAME_1)
         self.assertEqual('Saunatilan kaappi on täytetty.', strings.SLACK_MESSAGE_2)
         self.assertEqual('Täyttäminen vaiheessa.', strings.SLACK_MESSAGE_3)
         self.assertEqual('Otit vastuun täyttämisestä, viesti laitettu Slackiin.', strings.SUCCESS_MSG_3)
-
-
-if __name__ == '__main__':
-    unittest.main()
