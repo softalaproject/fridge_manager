4fb5f018c574f47886b7d909cd8f22dc7848dd9e
Ari Aaltonen <aaltonengithub@gmail.com>
2020-04-16 15:18:23 +0300

    Revert "Revert commit: b4b7b30"
    This reverts commit 15952293c31d29f2744c67780e4b1dd19682f99f.

--- restmanager/frontend/urls.py
+++ restmanager/frontend/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 # Defining URL endpoints and what views they should return.
 urlpatterns = [
-    path('', views.fridge),
+    path('', views.fridge, name="home"),
+    path('items', views.items),
     path('manage', views.manage),
-    path('api/beer/', views.post_beer),
-    path('api/no_beer/', views.post_no_beer),
+    path('api/beer/', views.post_beer, name="full"),
+    path('api/no_beer/', views.post_no_beer, name="empty"),
     path('fridge', views.fridge),
+    path('api/test_method/', views.test_method),
+    path('api/change_method/', views.change_method),
+    path('api/change_to_empty/', views.change_to_empty),
+    path('api/change_item/', views.change_item),
+
 ]
