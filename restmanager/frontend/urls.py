a77717081df11ae445f8a3a3d9b3b92c6be93e77
Ari Aaltonen <aaltonengithub@gmail.com>
2020-04-22 03:44:57 +0300

    Add api/beer and nobeer back

--- restmanager/frontend/urls.py
+++ restmanager/frontend/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('fridges', views.fridges),
     path('fridges/', views.fridges),
     path('fridge', views.fridge),
+    path('api/beer/', views.post_beer, name="full"),
+    path('api/no_beer/', views.post_no_beer, name="empty"),
     path('api/change_state/', views.change_state),
     path('create_fridges/', views.create_fridges),
 ]
