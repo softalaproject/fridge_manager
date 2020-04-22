e402db5a50073ea031dd3ea28c7880c841993fd1
Ari Aaltonen <aaltonengithub@gmail.com>
2020-04-22 13:52:47 +0300

    Add save fridges back

--- restmanager/frontend/urls.py
+++ restmanager/frontend/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('fridges/', views.fridges),
     path('fridge', views.fridge),
     path('api/change_state/', views.change_state),
+    path('create_fridges/', views.create_fridges),
 ]
