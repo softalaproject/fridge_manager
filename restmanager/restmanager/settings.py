50b5f1bcfc67c78c2ded36bba9d132da20dac394
nikke845 <nvuorivirta@gmail.com>
2020-04-24 11:48:29 +0300

    testing

--- restmanager/restmanager/settings.py
+++ restmanager/restmanager/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
+        "DEFAULT-CHARACTER-SET": 'utf8',
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT"),
     }
