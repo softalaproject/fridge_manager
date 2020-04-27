89348d8d161c722e55e53f186529b53162c90961
nikke845 <nvuorivirta@gmail.com>
2020-04-27 20:41:40 +0300

    docker-compose down > kill

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,13 +8,15 @@ ssh-add .travis/id_rsa # Add the private key to SSH
 # git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 # git push deploy dev
 
+# docker kill mariadb_django_mariadb-django_1
+# docker rm mariadb_django_mariadb-django_1
+
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh apps@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
-  docker kill mariadb_django_mariadb-django_1
-  docker rm mariadb_django_mariadb-django_1
   git fetch origin
   git reset --hard origin/feature/mariadb
   docker-compose build
+  docker-compose down
   docker-compose -p mariadb_django up -d
 EOF
