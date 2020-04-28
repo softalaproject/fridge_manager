2915efea6a4767cd347bdacb3f126cc20b7a3fc4
Ari Aaltonen <aaltonengithub@gmail.com>
2020-04-28 23:55:53 +0300

    Refactoring

--- restmanager/frontend/urls.py
+++ restmanager/frontend/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path('fridges', views.fridges),
     path('fridges/', views.fridges),
     path('api/change_state/', views.change_state),
-    path('create_fridges/', views.create_fridges),
-    path('create_fridges2/', views.create_fridges2),
 ]
 
