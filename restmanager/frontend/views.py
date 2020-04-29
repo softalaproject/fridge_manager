90185d0da6ff93dfaf9b23a6965d564c2d004eb9
Ari Aaltonen <aaltonengithub@gmail.com>
2020-04-29 23:29:51 +0300

    Added comments

--- restmanager/frontend/views.py
+++ restmanager/frontend/views.py
@@ -8,40 +8,51 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect
 
-# GET SLACK TOKEN HERE
+# Gets slack token from .env file and sets it here
 load_dotenv()
 client = slack.WebClient(token=os.getenv("SLACK_TOKEN"))
 
+# IP2 is the servers IP Address set in .env found in fridge_manager/restmanager
 IP2 = os.getenv('IP2')
+# D_PORT is set only here, determines which port the get_request is sent to (the port that the server is running on)
 D_PORT = "8100"
 
 
+# Creates a list of unique floors found in fridges table in the database and returns it
 @csrf_exempt
 def create_floor_list():
     floor_list = []
 
+    # adds unique floor numbers found in the get_request()
     for item in get_request():
         if item['floor'] not in floor_list:
             floor_list.append(item['floor'])
+    # sorts the floor_list from smallest to largest so the list makes sense in the view
     floor_list.sort()
     return floor_list
 
 
+# Sends get request to api endpoint /api/fridges/ which returns a json object with all the fridges in the database
 @csrf_exempt
 def get_request():
     r = requests.get('HTTP://' + IP2 + ':' + D_PORT + '/api/fridges/?format=json')
     return json.loads(r.text)
 
 
+# Creates a list and sorts through it depending on what parameters are given in the request that it receives
+# Accepts parameters from url id, floor, state and returns a select_list named list
 @csrf_exempt
 def create_list(request):
+    # Creates list to output
     select_list = []
+    # Sets the parameters found in the request
     floor = request.GET.get('floor')
     fridge_id = request.GET.get('id')
     state = request.GET.get('state')
-    channel_msg = request.GET.get('channel_msg')
 
+    # loops through data from get_request()
     for item in get_request():
+        # if the request has a given parameter floor it is added to select_list
         if floor is not None:
             int_floor = int(floor)
             if item['floor'] == int_floor:
@@ -53,27 +64,31 @@ def create_list(request):
         elif state is not None:
             if item['state'].lower() == state.lower():
                 select_list.append(item)
-        elif channel_msg is not None:
-            if item['channel_msg'].lower() == channel_msg.lower():
-                select_list.append(item)
+        # if the request has no given parameters, adds all data from get_request() to select_list
         else:
             select_list.append(item)
     return select_list
 
 
+# converts given list into json and returns it
 @csrf_exempt
 def create_json(a_list):
-    my_json_string = json.dumps(a_list)
-    return my_json_string
+    json_string = json.dumps(a_list)
+    return json_string
 
 
+# view found at /api/json/ uses the create_list function, meaning it can use all the parameters listed in the function
 @csrf_exempt
 def json_view(request):
+    # assigns the list using the create_list function
     filtered_list = create_list(request)
+    # creates the json to return using the create_json function
     json_response = create_json(filtered_list)
     return HttpResponse(json_response)
 
 
+# View for index and /floors/ endpoint, passes create_floor_list() which creates a list of unique floors
+# and passes it as context to template floors.html
 @csrf_exempt
 def floors(request):
     context = {
@@ -82,7 +97,8 @@ def floors(request):
     return render(request, 'frontend/floors.html', context)
 
 
-# Endpoint http://localhost:PORTNO/fridges.
+# View for /fridges/ endpoint, uses create_list() passing the received request to it and then passing the filtered list
+# as context to the template fridges.html
 @csrf_exempt
 def fridges(request):
     context = {
@@ -92,27 +108,41 @@ def fridges(request):
     return render(request, 'frontend/fridges.html', context)
 
 
+# View for endpoint /api/change_state, which is used to change the state of a fridge and send the message to slack
 @csrf_exempt
 def change_state(request):
+    # If the requests method which is sent to the endpoint is POST goes into the if logic
+    # otherwise redirects back to where the user came from
     if request.method == 'POST':
+        # sets the parameters from the received request
         fridge_name = request.POST.get('name')
         fridge_id = request.POST.get('id')
         floor_id = request.POST.get('floor')
         channel_msg = request.POST.get('channel_msg')
+        # sets username based on the given parameters which are sent in a form from the template
         username_c = 'Floor: ' + floor_id + ', ' + fridge_name
 
+        # if given state is empty changes to full, etc.
         if request.POST.get('state') == 'Empty':
             new_state = 'Full'
         elif request.POST.get('state') == 'Full':
             new_state = 'Half-full'
+        # if given state is anything else changes it to empty
         else:
             new_state = 'Empty'
 
+        # updates the fridge objects data in the database with given parameters
         Fridge.objects.filter(id=fridge_id).update(state=new_state)
+        # sends the message to slack
         client.chat_postMessage(
+            # sends the message to the channel corresponding with the requests given channel_msg value
             channel=f'#{channel_msg}',
+            # the text sent to slack, updating the state
             text=f'State: {new_state}',
+            # sets the username for the message which was given earlier in the view
             username=username_c
         )
+    # redirects the requests sender back to where they came from
+    # doesnt work if the user has blocked metadata with incognito mode or other means
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
 
