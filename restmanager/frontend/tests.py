e9f563a9128266c345dcdbed94cfcf9c2a552529
Ari Aaltonen <aaltonengithub@gmail.com>
2020-04-29 14:15:12 +0300

    Remove unused import

--- restmanager/frontend/tests.py
+++ restmanager/frontend/tests.py
@@ -1,7 +1,6 @@
 from django.urls import reverse, resolve
 from unittest import TestCase
 from .views import fridges
-from . import channels
 import unittest
 
 
@@ -12,11 +11,6 @@ class UrlsTest(TestCase):
         self.assertEquals(url, url2)
         self.assertEquals(resolve(url).func, fridges)
         self.assertEquals(resolve(url2).func, fridges)
-
-
-class UnitTests(TestCase):
-    def test_strings(self):
-        self.assertEqual('#general', channels.CHANNEL_NAME_1)
 
 
 if __name__ == '__main__':
