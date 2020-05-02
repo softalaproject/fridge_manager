e39582f4311f47895048f0c0b0655788fdb6111e
Ari Aaltonen <aaltonengithub@gmail.com>
2020-05-02 21:31:30 +0300

    use SQlite

--- restmanager/restmanager/settings.py
+++ restmanager/restmanager/settings.py
@@ -79,13 +79,8 @@ WSGI_APPLICATION = 'restmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        "DEFAULT-CHARACTER-SET": 'utf8',
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
