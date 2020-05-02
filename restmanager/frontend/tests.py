f55382b5ccc89a1891fef44d6ec96cdaaccf1cae
Ari Aaltonen <aaltonengithub@gmail.com>
2020-05-03 01:24:57 +0300

    Add a few tests

--- restmanager/frontend/tests.py
+++ restmanager/frontend/tests.py
@@ -1,6 +1,6 @@
 from django.urls import reverse, resolve
 from unittest import TestCase
-from .views import fridges
+from .views import fridges, floors, json_view
 import unittest
 
 
@@ -11,6 +11,20 @@ class UrlsTest(TestCase):
         self.assertEquals(url, url2)
         self.assertEquals(resolve(url).func, fridges)
         self.assertEquals(resolve(url2).func, fridges)
+
+    def test_floors(self):
+        url = '/floors/'
+        url2 = reverse(floors)
+        self.assertEquals(url, url2)
+        self.assertEquals(resolve(url).func, floors)
+        self.assertEquals(resolve(url2).func, floors)
+    
+    def test_api_json(self):
+        url = '/api/json/'
+        url2 = reverse(json_view)
+        self.assertEquals(url, url2)
+        self.assertEquals(resolve(url).func, json_view)
+        self.assertEquals(resolve(url2).func, json_view)
 
 
 if __name__ == '__main__':
