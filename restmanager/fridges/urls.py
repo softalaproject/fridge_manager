ab1d503df3c54ea98841e30ebbaf8369a6abc456
Claymonte <kaswic@hotmail.com>
2020-05-06 12:11:22 +0300

    more api versioning settings

--- restmanager/fridges/urls.py
+++ restmanager/fridges/urls.py
@@ -1,5 +1,10 @@
 from rest_framework import routers
 from .api import FridgeViewSet
+from django.urls import path, re_path
+from django.conf.urls import url
+
+url(r'^(?P<version>(v1))/fridges/$', views.fridges.as_view()),
+re_path(r'^(?P<version>(v1))/fridges/(?P<pk>\d+)/$', views.fridges.as_view()),
 
 router = routers.DefaultRouter()
 # registers the api/fridges endpoint and maps it to use FridgeViewSet
