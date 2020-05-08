ce79cbb8e664b511f777a2729cf1418df2da6893
nikke845 <nvuorivirta@gmail.com>
2020-05-08 12:57:35 +0300

    docker-compose down before build

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,9 +7,9 @@ ssh-add .travis/id_rsa # Add the private key to SSH
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh apps@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
+  docker-compose down
   git fetch origin
   git reset --hard origin/master
   docker-compose build
-  docker-compose down
   docker-compose up -d
 EOF
