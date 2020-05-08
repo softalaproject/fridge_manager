ef746f51a0c22f1c46ca7bbb96e45a62d5e3fa43
Ari Aaltonen <aaltonengithub@gmail.com>
2020-05-08 11:22:59 +0300

    change getenvs

--- restmanager/restmanager/settings.py
+++ restmanager/restmanager/settings.py
@@ -88,9 +88,9 @@ WSGI_APPLICATION = 'restmanager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'NAME': os.getenv("MYSQL_DATABASE"),
+        'USER': os.getenv("MYSQL_USER"),
+        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
         "DEFAULT-CHARACTER-SET": 'utf8',
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT"),
