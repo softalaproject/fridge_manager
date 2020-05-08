9e292e8f1239b73de139197d5d741583bf399d89
Lauri Leinonen <leinonen.lauri7@gmail.com>
2020-05-08 12:25:47 +0300

    collectstatic with whitenoise tested

--- restmanager/restmanager/settings.py
+++ restmanager/restmanager/settings.py
@@ -45,13 +45,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'restmanager.urls'
@@ -138,4 +138,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, './frontend/static')
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
